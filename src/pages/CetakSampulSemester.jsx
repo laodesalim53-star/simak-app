cd0d3928ee7d984a121f11472cf91eb8c838c7c3
laodesalim53-star <laodesalim53@gmail.com>
2026-09-11 03:47:08 +0900

    Create CetakSampulSemester.jsx

---
+++ src/pages/CetakSampulSemester.jsx
@@ -0,0 +1,16 @@
+import SampulLaporan, { JENIS_LAPORAN_PRESET } from '../components/SampulLaporan'
+
+// Cabang khusus dari CetakSampul.jsx: jenis laporan dikunci ke "Laporan Semester",
+// jadi guru/admin tidak perlu memilih dari dropdown lagi.
+export default function CetakSampulSemester() {
+  return (
+    <SampulLaporan
+      jenisLaporanAwal={JENIS_LAPORAN_PRESET[1] /* 'Laporan Semester' */}
+      kunciJenisLaporan
+      subJudulAwal="LAPORAN HASIL BELAJAR PESERTA DIDIK"
+      labelTahun="Tahun Ajaran"
+      tampilkanBank
+      labelHalaman="Cetak Sampul Laporan Semester"
+    />
+  )
+}
