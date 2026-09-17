d983613bcbc9c30f5e0f17e1c52685e2196e60da
laodesalim53-star <laodesalim53@gmail.com>
2026-09-17 15:30:13 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -270,7 +270,7 @@ export default function App() {
           <Route path="/presensi-kantor" element={<ProtectedRoute adminOnly><PresensiKantor /></ProtectedRoute>} />
           <Route path="/daftar-hadir-kantor" element={<ProtectedRoute adminOnly><DaftarHadirKantor /></ProtectedRoute>} />
           <Route path="/data-pegawai-kantor" element={<ProtectedRoute adminOnly><DataPegawaiKantor /></ProtectedRoute>} />
-          <Route path="/pusat-materi-majelis" element={<ProtectedRoute adminOnly><PusatMateriMajelis /></ProtectedRoute>} />
+          <Route path="/pusat-materi-majelis" element={<ProtectedRoute><PusatMateriMajelis /></ProtectedRoute>} />
           <Route path="/daftar-hadir-pegawai" element={<ProtectedRoute adminOnly><DaftarHadirPegawai /></ProtectedRoute>} />
           <Route path="/materi-keluarga-sakinah" element={<ProtectedRoute adminOnly><MateriKeluargaSakinah /></ProtectedRoute>} />
           <Route path="/materi-pengelolaan-zakat" element={<ProtectedRoute adminOnly><MateriPengelolaanZakat /></ProtectedRoute>} />
@@ -280,7 +280,7 @@ export default function App() {
           <Route path="/laporan-masyarakat-bermoral-harmonis" element={<ProtectedRoute adminOnly><LaporanPenyuluhanMasyarakatBermoral /></ProtectedRoute>} />
           <Route path="/rencana-kerja-tahunan" element={<ProtectedRoute><RencanaKerjaTahunan /></ProtectedRoute>} />
           <Route path="/materi-majelis-taklim" element={<ProtectedRoute adminOnly><MateriMajelisTaklim /></ProtectedRoute>} />
-          <Route path="/pusat-kelompok-binaan" element={<ProtectedRoute adminOnly><PusatKelompokBinaan /></ProtectedRoute>} />
+          <Route path="/pusat-kelompok-binaan" element={<ProtectedRoute><PusatKelompokBinaan /></ProtectedRoute>} />
           <Route path="/kelompok-binaan/:slug" element={<ProtectedRoute adminOnly><KelolaKelompokBinaan /></ProtectedRoute>} />
           <Route path="/kelas" element={<ProtectedRoute adminOnly><Kelas /></ProtectedRoute>} />
           <Route path="/profil-kantor" element={<ProtectedRoute adminOnly><ProfilKantor /></ProtectedRoute>} />
